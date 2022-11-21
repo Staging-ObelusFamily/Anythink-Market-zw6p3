0c06fd0f38db82ceb81bfc1b94517fdc2c4427b0
Shem <smagnezi8@gmail.com>
2022-11-21 15:35:32 +0000

    test

--- frontend/src/components/Item/CommentInput.js
+++ frontend/src/components/Item/CommentInput.js
@@ -34,7 +34,7 @@ class CommentInput extends React.Component {
       <form className="card comment-form m-2" onSubmit={this.createComment}>
         <div className="card-block">
           <textarea
-            className="form-control"
+            className="form-control test"
             placeholder="Write a comment..."
             value={this.state.body}
             onChange={this.setBody}
